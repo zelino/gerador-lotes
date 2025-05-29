9ae6e16cccffb11565859feeb52d5b360859e876
Zelino Jr <zlnihues@gmail.com>
2025-05-29 14:51:15 -0300

    feat: update database connection string and add initial components for login and error handling

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -171,12 +171,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://lotes:hzURMxmcRStB@localhost:5432/db_lotes?schema=public"
       }
     }
   },
